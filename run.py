319e93acc4c15af2ac251c71af476862fe9d01e0
DazHaze <dhayes1.wex@gmail.com>
2021-11-21 19:43:48 +0000

    initial heroku setup

--- run.py
+++ run.py
@@ -8,60 +8,74 @@ class Board:
     """
     Creates a Connect4 Board
     """
+
     def __init__(self):
         self.row_count = 6
         self.col_count = 7
         self.board = np.zeros((self.row_count, self.col_count))
 
-
     # Remember to flip board
+
+    def display_upsidedown_board(self):
+        print(np.flip(self.board, 0))
+
     def display_board(self):
         print(self.board)
 
     def update_board(self, row, col, piece):
         self.board[row][col] = piece
 
-    def valid_drop(self, col):
+    def valid_drop(self):
         global empty_row
+        global choice
+        choice = int(input("Please choose a column: \n"))
         for row in range(self.row_count):
-            if self.board[row][col] == 0:
+            if self.board[row][choice] == 0:
                 empty_row = row
-                self.update_board(empty_row, col, 7)
-                print(empty_row)
+                self.update_board(empty_row, choice, 7)
                 break
             elif row == self.row_count-1:
-                print("Column is full, please choose another.")
+                print("Column is full, please choose another.\n")
+                choice = int(input("Please choose a column: \n"))
                 break
 
-    def check_win_columns(self):
+    def check_win(self):
+        # Check win in columns
         for row in range(self.row_count):
             for col in range(self.col_count):
                 if self.board[row][col] == 7:
                     if row + 3 < 6:
                         if self.board[row][col] == self.board[row+1][col] == self.board[row+2][col] == self.board[row+3][col]:
-                            print("Win columns")
-    
+                            return True
+        # Check win in rows
+        for col in range(self.col_count):
+            for row in range(self.row_count):
+                if self.board[row][col] == 7:
+                    if col + 3 < 7:
+                        if self.board[row][col] == self.board[row][col+1] == self.board[row][col+2] == self.board[row][col+3]:
+                            return True
+
     def check_win_rows(self):
         for col in range(self.col_count):
             for row in range(self.row_count):
                 if self.board[row][col] == 7:
                     if col + 3 < 7:
                         if self.board[row][col] == self.board[row][col+1] == self.board[row][col+2] == self.board[row][col+3]:
-                            print("Win rows")
-
-
+                            return True
 
 
 gameBoard = Board()
 
-gameBoard.update_board(2, 0, 7)
-gameBoard.update_board(2, 1, 7)
-gameBoard.update_board(2, 2, 7)
-gameBoard.update_board(2, 3, 7)
-
-
-
-user_choice = int(input("Player 1, Make your Selection(0-6):"))
-gameBoard.valid_drop(user_choice)
-print(gameBoard.display_board())
-gameBoard.check_win_rows()
+playing = True
+while playing:
+    win = False
+    while not win:
+        gameBoard.display_upsidedown_board()
+        gameBoard.valid_drop()
+        if gameBoard.check_win():
+            win = True
+            print("You won!")
+        else:
+            win = False
+    else:
+        playing = False
